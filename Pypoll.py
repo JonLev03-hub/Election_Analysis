18b8b2f65c5fd0ce97207fe1b00e776b231a5bf9
Jonathan Leverenz <jonlev03@gmail.com>
2021-10-07 18:11:08 -0400

    Created Pseudocode for script

---
+++ Pypoll.py
@@ -0,0 +1,10 @@
+#Goals of this code 1. count total votes 2. count votes per candidate 3. count % vote per candidate 4. calculate winner based on popular vote
+#
+# innitialize a dictionary for candidates votes
+#
+# for every row
+#   -if candidate is in the dictionary add a vote to them 
+#   -else add them to the dictionary and give them one vote 
+#
+# print total votes, print votes per candidate and % per candidate in the same line,
+# print key with largest value 
